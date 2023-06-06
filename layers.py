37eb0398dd466bd4392adc756d99ad03b6e1a8f0
CatherChen <58179708+Cather-Chen@users.noreply.github.com>
2023-06-06 14:17:39 -0700

    Update layers.py

--- layers.py
+++ layers.py
@@ -20,7 +20,7 @@ class GraphConvolution(Module):
             self.register_parameter('bias', None)
         self.reset_parameters()
 
-    def reset_parameters(self):  #初始化参数
+    def reset_parameters(self):
         stdv = 1. / math.sqrt(self.weight.size(1))
         self.weight.data.uniform_(-stdv, stdv)
         if self.bias is not None:
@@ -31,12 +31,12 @@ class GraphConvolution(Module):
         adj_norm = torch.zeros_like(adj).to(adj.device)
         for bs in range(bsz):
             mx = adj[bs]
-            rowsum = mx.sum(1)  # 每行的数加在一起
-            r_inv_sqrt = torch.pow(rowsum, -0.5).flatten()  # 输出rowsum ** -1/2
-            r_inv_sqrt[torch.isinf(r_inv_sqrt)] = 0.  # 溢出部分赋值为0
-            r_mat_inv_sqrt = torch.diag(r_inv_sqrt)  # 对角化
+            rowsum = mx.sum(1)
+            r_inv_sqrt = torch.pow(rowsum, -0.5).flatten()
+            r_inv_sqrt[torch.isinf(r_inv_sqrt)] = 0.
+            r_mat_inv_sqrt = torch.diag(r_inv_sqrt)
             mx = torch.matmul(r_mat_inv_sqrt,mx)
-            mx = torch.transpose(mx, 0, 1)  # 转置
+            mx = torch.transpose(mx, 0, 1)
             mx = torch.matmul(mx, r_mat_inv_sqrt)
             mx = torch.transpose(mx,0,1)
         adj_norm[bs] = mx
@@ -81,7 +81,6 @@ class SelfAttention_ori(Module):
         self.n = x.size()[0]
         x = torch.cat([x, torch.stack([x] * self.n, dim=0)], dim=2)
         U = torch.matmul(x, self.a).transpose(0, 1)
-        # 非线性激活
         U = F.leaky_relu(U)
         weights = F.softmax(U, dim=1)
         outputs = torch.matmul(weights.transpose(1, 2), inputs).squeeze(1)
@@ -110,18 +109,14 @@ class SelfAttention(Module):
         self.a.data.uniform_(-stdv, stdv)
 
     def forward(self, inputs):
-        # inputs size:  bsz,2,node_num,in_features
         x = self.linear(inputs)  # x: (bsz*4,2,n_nodes,hidden_dim)
         self.n = x.size()[1]
         x = torch.cat([x, torch.stack([x[:,self.idx,:,:]] * self.n, dim=1)], dim=3)
-        # x: (bsz*4,2,n_nodes,2* hidden_dim)
         outputs = torch.zeros(x.size()[0],x.size(2),inputs.size(-1)).to(inputs.device)   #(bsz*4,n_nodes,in_features)
         weights_ = torch.zeros(x.size(0),x.size(2),x.size(1), 1).to(inputs.device)  #(bsz*4,n_nodes,2,1)
         for bs in range(x.size()[0]):
             x_ = x[bs]
             U = torch.matmul(x_,self.a).transpose(0, 1)  #(n_nodes,2,1)
-            # U = self.norm(U)
-            # 非线性激活
             U = F.leaky_relu_(U)
             weights = F.softmax(U, dim=1)
             weights = torch.where(torch.isnan(weights), torch.full_like(weights, 0), weights)
@@ -130,31 +125,6 @@ class SelfAttention(Module):
             outputs[bs] = outputs_bs
             weights_[bs] = weights
         return outputs, weights_
-
-
-
-# class SelfAttention(Module):
-#     """docstring for SelfAttention"""
-#     def __init__(self, in_features, idx):
-#         super(SelfAttention, self).__init__()
-#         self.idx = idx
-#         self.w = Parameter(torch.FloatTensor(in_features, in_features))
-#         self.reset_parameters()
-
-#     def reset_parameters(self):
-#         stdv = 1. / math.sqrt(self.w.size(1))
-#         self.w.data.uniform_(-stdv, stdv)
-
-#     def forward(self, inputs):
-#         # inputs' shape is like 4*d, w's shape is like d*d
-#         # u's shape is like 4*4
-#         U = torch.matmul(inputs, self.w)
-#         U = torch.matmul(U, inputs.transpose(1, 2))
-#         # 非线性激活
-#         U = F.tanh(U)
-#         weights = F.softmax(U.transpose(0, 1)[self.idx], dim=1)
-#         outputs = torch.matmul(torch.stack([weights], dim=1), inputs).squeeze(1)
-#         return outputs, weights
 
 class MLP(Module):
     """docstring for MLP"""
@@ -245,24 +215,15 @@ class GraphAttentionConvolution(Module):
         for t1 in range(self.ntype):
             x_t1 = []
             for t2 in range(self.ntype):
-
-                # if adj_list[t1][t2] == :
-                #     x_t1.append(torch.zeros(adj_list[t1][t2].shape[0], self.out_features, device=self.bias.device))
-                #     continue
-                #     # print('error.')
-                #
                 if self.bias is not None:
-                    # x_t1.append( self.att(h[t1], h[t2], adj_list[t1][t2]) + self.bias )
                     x_t1.append(self.att_list[t1](h[t1], h[t2], adj_list[t1][t2]) + self.bias)
                 else:
-                    # x_t1.append( self.att(h[t1], h[t2], adj_list[t1][t2]) )
                     x_t1.append(self.att_list[t1](h[t1], h[t2], adj_list[t1][t2]))
             outputs.append(x_t1)
             
         return outputs
 class Attention_InfLevel(nn.Module):
     '''
-    计算input1 和 input2 之间的attention，并返回input1的加权更新值(维度为dim_features)
     input1 & input2 :(bsz*4, n_nodes, out_feature_dim)
     adj_matrix: (bsz*4,n1_nodes, n2_nodes)
     Here, dim_feature = out_feature_dim
@@ -307,16 +268,16 @@ class Attention_InfLevel(nn.Module):
         adj_ = torch.zeros_like(attention).to(input1.device)
         for bs in range(adj_.size(0)):
             adj_[bs] = adj[bs].sum(1).repeat(M, 1).T  #[n1,n2]
-        attention = torch.mul(attention, adj_)  # (bsz*4,n1_nodes, n2_nodes)对应位相乘
+        attention = torch.mul(attention, adj_)
         # attention = F.dropout(attention, self.dropout, training=self.training)
 
         del(zero_vec)
 
         # attention = F.dropout(attention, self.dropout, training=self.training)
-        h_prime = torch.matmul(attention, g)  # (bsz*4,n1_nodes, n2_nodes) * (bsz*4, n2_nodes, out_feature_dim)
-        return h_prime  #(bsz*4,n1_nodes,out_feature_dim)
+        h_prime = torch.matmul(attention, g)
+        return h_prime 
 
-class FFNLayer(nn.Module):  #全连接层，由input_dim → intermediate_dim → output_dim
+class FFNLayer(nn.Module):
     def __init__(self, input_dim, intermediate_dim, output_dim, dropout, layer_norm=True):
         super(FFNLayer, self).__init__()
         self.fc1 = nn.Linear(input_dim, intermediate_dim)
@@ -377,39 +338,36 @@ class ArgumentGCN(nn.Module):
 
         node_len = node.size(1) # n_nodes
 
-        diagmat = torch.diagflat(torch.ones(node.size(1), dtype=torch.long, device=node.device))  # 生成对角线为1的方阵 n * n, n = n_nodes
-        diagmat = diagmat.unsqueeze(0).expand(node.size(0), -1, -1)  # size = (bsz x n_choices, n_nodes, n_nodes) 第一维方向上复制
+        diagmat = torch.diagflat(torch.ones(node.size(1), dtype=torch.long, device=node.device))
+        diagmat = diagmat.unsqueeze(0).expand(node.size(0), -1, -1) 
         dd_graph = node_mask.unsqueeze(-1) * node_mask.unsqueeze(1) * (1 - diagmat)
-        #(bsz,1,n_nodes)*(bsz,n_nodes,1) = (bsz,n_node,n_node)  对角线处取0，非对角线处
         graph_argument = dd_graph * argument_graph
         graph_punctuation = dd_graph * punctuation_graph
         # graph_argument, graph_punctuation shape = (bsz x n_choices, n_node, n_node)
         node_neighbor_num = graph_argument.sum(-1) + graph_punctuation.sum(-1)  # shape=(bsz x n_choices, n_nodes)
-        node_neighbor_num_mask = (node_neighbor_num >= 1).long()  #有neighbor连接的点为1，没连接的为0  shape=(bsz x n_choices, n_nodes)
-        node_neighbor_num = util.replace_masked_values(node_neighbor_num.float(), node_neighbor_num_mask, 1)  # 没有边连接的点都记为1个边连接
+        node_neighbor_num_mask = (node_neighbor_num >= 1).long()
+        node_neighbor_num = util.replace_masked_values(node_neighbor_num.float(), node_neighbor_num_mask, 1)
 
         all_weight = []
         for step in range(self.iteration_steps):
 
             ''' (1) Node Relatedness Measure '''
             if extra_factor is None:
-                d_node_weight = torch.sigmoid(self._node_weight_fc(node)).squeeze(-1)  #(bsz x n_choices, n_nodes) 映到1维，并删去最后一维的表示
+                d_node_weight = torch.sigmoid(self._node_weight_fc(node)).squeeze(-1)
             else:
                 d_node_weight = torch.sigmoid(self._node_weight_fc(torch.cat((node, extra_factor), dim=-1))).squeeze(-1)
 
-            all_weight.append(d_node_weight)   #每个step都append一次weight
+            all_weight.append(d_node_weight)
 
             self_node_info = self._self_node_fc(node)  # (bsz x n_choices, n_nodes, embed_size)
 
             ''' (2) Message Propagation (each relation type) '''
-            # 获取 argument_graph的更新信息
             node_info_argument = self._node_fc_argument(node) # (bsz x n_choices, n_nodes, emb_size)
             node_weight = util.replace_masked_values(
-                d_node_weight.unsqueeze(1).expand(-1, node_len, -1), #(bsz x n_choices, n_nodes, n_nodes) 第二维复制
-                graph_argument, 0)     #前tensor 在 graph_argument 中被masked的位置处用0替代
+                d_node_weight.unsqueeze(1).expand(-1, node_len, -1),
+                graph_argument, 0) 
             node_info_argument = torch.matmul(node_weight, node_info_argument) # (bsz x n_choices, n_nodes, emb_size)
 
-            # 获取 punctuation_graph的更新信息
             node_info_punctuation = self._node_fc_punctuation(node)
             node_weight = util.replace_masked_values(
                 d_node_weight.unsqueeze(1).expand(-1, node_len, -1),
@@ -435,7 +393,7 @@ class ArgumentGCN(nn.Module):
 def masked_softmax(vector: torch.Tensor,
                    mask: torch.Tensor,
                    dim: int = -1,
-                   memory_efficient: bool = True,#False,
+                   memory_efficient: bool = True,
                    mask_fill_value: float = -1e32) -> torch.Tensor:
     if mask is None:
         result = torch.nn.functional.softmax(vector, dim=dim)
@@ -483,23 +441,20 @@ class GraphAttentionLayer(nn.Module):
         self.out_features = out_features
         self.alpha = alpha
         self.concat = concat
-        # 创建空向量并初始化  W矩阵：infea → outfea
         self.W = nn.Parameter(torch.empty(size=(in_features, out_features)))
         nn.init.xavier_uniform_(self.W.data, gain=1.414)
-        # 创建空向量并初始化  a矩阵： （2*outfea） * 1
         self.a = nn.Parameter(torch.empty(size=(2 * out_features, 1)))
         nn.init.xavier_uniform_(self.a.data, gain=1.414)
 
         self.leakyrelu = nn.LeakyReLU(self.alpha)
 
-    def forward(self, h, adj):  # adj：邻接矩阵
+    def forward(self, h, adj):
         Wh = torch.mm(h, self.W)  # h.shape: (N, in_features), Wh.shape: (N, out_features)
         a_input = self._prepare_attentional_mechanism_input(Wh)
-        # 实现Whi和Whj concatenation  shape  = (N, N, 2 * self.out_features)
-        e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(2))  # 激活后去掉最后两个维度
+        e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(2))
 
         zero_vec = -9e15 * torch.ones_like(e)
-        attention = torch.where(adj > 0, e, zero_vec)  # 不邻接的元素设为-∞
+        attention = torch.where(adj > 0, e, zero_vec)
         attention = F.softmax(attention, dim=1)
         attention = F.dropout(attention, self.dropout, training=self.training)
         h_prime = torch.matmul(attention, Wh)  # shape = (N, out_features)
@@ -511,39 +466,9 @@ class GraphAttentionLayer(nn.Module):
 
     def _prepare_attentional_mechanism_input(self, Wh):
         N = Wh.size()[0]  # number of nodes
-
-        # Below, two matrices are created that contain embeddings in their rows in different orders.
-        # (e stands for embedding)
-        # These are the rows of the first matrix (Wh_repeated_in_chunks):
-        # e1, e1, ..., e1,            e2, e2, ..., e2,            ..., eN, eN, ..., eN
-        # '-------------' -> N times  '-------------' -> N times       '-------------' -> N times
-        #
-        # These are the rows of the second matrix (Wh_repeated_alternating):
-        # e1, e2, ..., eN, e1, e2, ..., eN, ..., e1, e2, ..., eN
-        # '----------------------------------------------------' -> N times
-        #
-
         Wh_repeated_in_chunks = Wh.repeat_interleave(N, dim=0)
         Wh_repeated_alternating = Wh.repeat(N, 1)
-        # Wh_repeated_in_chunks.shape == Wh_repeated_alternating.shape == (N * N, out_features)
 
-        # The all_combination_matrix, created below, will look like this (|| denotes concatenation):
-        # e1 || e1
-        # e1 || e2
-        # e1 || e3
-        # ...
-        # e1 || eN
-        # e2 || e1
-        # e2 || e2
-        # e2 || e3
-        # ...
-        # e2 || eN
-        # ...
-        # eN || e1
-        # eN || e2
-        # eN || e3
-        # ...
-        # eN || eN
 
         all_combinations_matrix = torch.cat([Wh_repeated_in_chunks, Wh_repeated_alternating], dim=1)
         # all_combinations_matrix.shape == (N * N, 2 * out_features)
